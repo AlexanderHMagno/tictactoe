736d93b987e929f91506e6e9758fd0a92832d922
Alexander Hortua <alexander.hortua10@gmail.com>
2022-11-16 22:26:15 -0800

    Adding JavaDoc to this implementation

--- src/tictactoe/TicTacToeConsoleController.java
+++ src/tictactoe/TicTacToeConsoleController.java
@@ -3,13 +3,25 @@ package tictactoe;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Scanner;
 
+
+/**
+ * This Class represents the Tic Tac Toe Controller, it has a Readable and Appendable objects. It implements
+ * TicTacToeController Interface.
+ */
 public class TicTacToeConsoleController implements TicTacToeController{
     final Readable in;
     final Appendable out;
     private static final int placeholder = -9870;
+
+    /**
+     * Constructor it has a Readable and Appendable objects.
+     * @param in The source of the input.
+     * @param out The object to aggregate the information
+     */
     public TicTacToeConsoleController(Readable in, Appendable out) {
         this.in = in;
         this.out = out;
@@ -19,30 +31,26 @@ public class TicTacToeConsoleController implements TicTacToeController{
     public void playGame(TicTacToe m) throws IllegalStateException {
         Objects.requireNonNull(m);
         Scanner scan = new Scanner(this.in);
+
+        //Use this array to control the row and column inputs
         int[] data = new int[2];
 
         try {
-
             //We need a loop to control the rounds
             while (!m.isGameOver()) {
                 resetData(data);
                 this.printLine(m.toString());
                 this.printLine("Enter a move for " + m.getTurn().toString() + ":");
 
-                //Another loop to control the users input
+                //Another loop to control the users input, q|Q or integers
                 while (true) {
 
-                    if (scan.hasNext("q")) {
-                        this.printLine("Game quit! Ending game state:");
-                        this.printLine(m.toString());
-                        return;
-                    }
-
                     for (int i = 0; i < data.length; i++) {
-                        if (data[i] == placeholder) {
-                            if(scan.hasNextInt()) data[i] = scan.nextInt();
-                            else this.printLine("Not a valid number: " + scan.next());
+                        if (scan.hasNext("(?i)q")) {
+                            this.quitGame(m);
+                            return;
                         }
+                        this.inputValidation(data,i,scan);
                     }
 
                     //Verify we have a valid input
@@ -62,20 +70,50 @@ public class TicTacToeConsoleController implements TicTacToeController{
             this.printLine(m.toString());
             this.printLine("Game is over! " + (m.getWinner() == null ? "Tie game." :  m.getWinner() + " wins."));
 
-        } catch (IOException e) {
+        } catch (IOException | NoSuchElementException n) {
             throw new IllegalStateException();
         }
-
     }
 
-
-
+    /**
+     * Helper function to append information to the appendable with new lines
+     * @param line Information to append
+     * @throws IOException If invalid value is passed
+     */
     private void printLine (String line) throws IOException {
         this.out.append(line).append("\n");
     }
 
+    /**
+     * Helper function to reset the information of the array that holds each position
+     * @param data Position of the next element to play
+     */
     private void resetData (int[] data) {
         data[0] = placeholder;
         data[1] = placeholder;
+    }
+
+    /**
+     * Append information to terminate the game.
+     * @param m Tic Tac Toe Model
+     * @throws IOException If invalid value is passed
+     */
+    private void quitGame (TicTacToe m) throws IOException{
+        this.printLine("Game quit! Ending game state:");
+        this.printLine(m.toString());
+    }
+
+    /**
+     * Verify the input is a valid input otherwise display the text indicating the invalid input
+     * @param data coordinates of row and column
+     * @param index indicate if the number should be added to row (0) or column (1)
+     * @param scan The Readable to obtain users input
+     * @throws IOException If invalid value is passed
+     */
+    private void inputValidation (int[] data, int index, Scanner scan) throws IOException {
+        if (data[index] == placeholder) {
+            if(scan.hasNextInt()) data[index] = scan.nextInt();
+            else this.printLine("Not a valid number: " + scan.next());
+        }
     }
 }
--- test/TicTacToeControllerTest.java
+++ test/TicTacToeControllerTest.java
@@ -11,18 +11,6 @@ import org.junit.Test;
  */
 public class TicTacToeControllerTest {
 
-  // ADDITIONAL TEST CASES TO IMPLEMENT:
-  // Play game to completion, where there is a winner
-  // Input where the q comes instead of an integer for the row
-  // Input where the q comes instead of an integer for the column
-  // Input where non-integer garbage comes instead of an integer for the row
-  // Input where non-integer garbage comes instead of an integer for the column
-  // Input where the move is integers, but outside the bounds of the board
-  // Input where the move is integers, but invalid because the cell is occupied
-  // Multiple invalid moves in a row of various kinds
-  // Input including valid moves interspersed with invalid moves, game is played to completion
-  // What happens when the input ends "abruptly" -- no more input, but not quit, and game not over
-  // THIS IS NOT AN EXHAUSTIVE LIST
 
   @Test
   public void testSingleValidMove() {
@@ -52,6 +40,9 @@ public class TicTacToeControllerTest {
 
   }
 
+  /**
+   * Test If not valid data is provided for Row
+   */
   @Test
   public void testBogusInputAsRow() {
     TicTacToe m = new TicTacToeModel();
@@ -76,6 +67,9 @@ public class TicTacToeControllerTest {
     // note no trailing \n here, because of the earlier split
   }
 
+  /**
+   * Test the output of a tie game
+   */
   @Test
   public void testTieGame() {
     TicTacToe m = new TicTacToeModel();
@@ -89,6 +83,9 @@ public class TicTacToeControllerTest {
     assertEquals("Game is over! Tie game.", lines[lines.length - 1]);
   }
 
+  /**
+   * Test a Failing Appendable Error
+   */
   @Test(expected = IllegalStateException.class)
   public void testFailingAppendable() {
     TicTacToe m = new TicTacToeModel();
@@ -98,4 +95,153 @@ public class TicTacToeControllerTest {
     c.playGame(m);
   }
 
+  // Play game to completion, where there is a winner
+  @Test
+  public void testWinner () {
+    String lastMsg = this.runGameHelper("1 1 1 2 1 3 2 1 2 3 1 3 3 1 2 2 3 2 3 3", 0);
+    assertEquals(
+            " X | O | X\n" +
+            "-----------\n" +
+            " O | X | X\n" +
+            "-----------\n" +
+            " O | O | X\n" +
+            "Game is over! X wins.", lastMsg);
+
+  }
+  // Input where the q comes instead of an integer for the row
+  @Test
+  public void testQInRow () {
+
+    String lastMsg = this.runGameHelper("2 2 q 2 ", 0);
+    assertEquals("Game quit! Ending game state:\n"
+            + "   |   |  \n"
+            + "-----------\n"
+            + "   | X |  \n"
+            + "-----------\n"
+            + "   |   |  ", lastMsg);
+
+  }
+
+  // Input where the q comes instead of an integer for the column
+  @Test
+  public void testQInColumn () {
+    String lastMsg = this.runGameHelper("2 2 1 Q ", 0);
+    assertEquals("Game quit! Ending game state:\n"
+            + "   |   |  \n"
+            + "-----------\n"
+            + "   | X |  \n"
+            + "-----------\n"
+            + "   |   |  ", lastMsg);
+  }
+
+
+  // Input where non-integer garbage comes instead of an integer for the row
+  @Test
+  public void testGarbageRow () {
+
+    String lastMsg = this.runGameHelper("2 2 A B C D E F q", 6);
+    assertEquals(
+            "Not a valid number: A\n" +
+            "Not a valid number: B\n" +
+            "Not a valid number: C\n" +
+            "Not a valid number: D\n" +
+            "Not a valid number: E\n" +
+            "Not a valid number: F" , lastMsg);
+
+  }
+  // Input where non-integer garbage comes instead of an integer for the column
+  @Test
+  public void testGarbageColumn () {
+    String lastMsg = this.runGameHelper("2 2 1 A B C D E F q", 6);
+    assertEquals(
+    "Not a valid number: A\n" +
+            "Not a valid number: B\n" +
+            "Not a valid number: C\n" +
+            "Not a valid number: D\n" +
+            "Not a valid number: E\n" +
+            "Not a valid number: F" , lastMsg);
+
+  }
+  // Input where the move is integers, but outside the bounds of the board
+  @Test
+  public void testOutsideBoundaries () {
+      String lastMsg = this.runGameHelper("2 2 4 5 8 8 -1 -1 3 8 3 4 4 5 6 6 q", 6);
+      assertEquals(
+              "Not a valid move: 8, 8\n" +
+                      "Not a valid move: -1, -1\n" +
+                      "Not a valid move: 3, 8\n" +
+                      "Not a valid move: 3, 4\n" +
+                      "Not a valid move: 4, 5\n" +
+                      "Not a valid move: 6, 6"  , lastMsg);
+
+  }
+  // Input where the move is integers, but invalid because the cell is occupied
+  @Test
+  public void testTakenCell () {
+
+    String lastMsg = this.runGameHelper("2 2 1 1 3 3 2 2 1 1 3 3 2 2 1 1 3 3 q ", 6);
+    assertEquals(
+            "Not a valid move: 2, 2\n" +
+                    "Not a valid move: 1, 1\n" +
+                    "Not a valid move: 3, 3\n" +
+                    "Not a valid move: 2, 2\n" +
+                    "Not a valid move: 1, 1\n" +
+                    "Not a valid move: 3, 3" , lastMsg);
+
+  }
+  // Multiple invalid moves in a row of various kinds
+  @Test
+  public void testMultipleInvalidRows () {
+    String lastMsg = this.runGameHelper("2 2 1 1 3 3 a 1 b c 1a -100 -4 q", 6);
+    assertEquals(
+            "Enter a move for O:\n" +
+                    "Not a valid number: a\n" +
+                    "Not a valid number: b\n" +
+                    "Not a valid number: c\n" +
+                    "Not a valid number: 1a\n" +
+                    "Not a valid move: -100, 1", lastMsg);
+  }
+  // Input including valid moves interspersed with invalid moves, game is played to completion
+  @Test
+  public void testInterspersedMoves () {
+    String lastMsg = this.runGameHelper("1 1 1 1 2 2 2 2 3 3 3 3 1 3 1 3 2 3 2 3 2 1 2 1 3 1 3 1 3 2 3 2 1 2", 0);
+    assertEquals(
+            " X | X | O\n" +
+                    "-----------\n" +
+                    " O | O | X\n" +
+                    "-----------\n" +
+                    " X | O | X\n" +
+                    "Game is over! Tie game.", lastMsg);
+
+  }
+  // What happens when the input ends "abruptly" -- no more input, but not quit, and game not over
+  @Test (expected = IllegalStateException.class)
+  public void testTerminatedAbruptly () {
+    this.runGameHelper("1 1 2 2 2 3 3", 0);
+  }
+
+
+  /**
+   * Helper method to create a based on the gamePath that was provided
+   * @param gamePath Instructions to follow, each movement should be separated by a space
+   * @param shift From bottom to top it moves the returned string according to this input
+   * @return Return the 6 last elements of the gameLog (the shift argument will alter the position)
+   */
+  private String runGameHelper(String gamePath, int shift) throws IllegalArgumentException {
+
+    //Provide a valid path to create the game
+    if (gamePath.length() == 0) throw new IllegalArgumentException();
+
+    TicTacToe m = new TicTacToeModel();
+    // note the entire sequence of user inputs for the entire game is in this one string:
+    StringReader input = new StringReader(gamePath);
+    StringBuilder gameLog = new StringBuilder();
+    TicTacToeController c = new TicTacToeConsoleController(input, gameLog);
+    c.playGame(m);
+
+    String[] lines = gameLog.toString().split("\n");
+
+    return String.join("\n",
+            Arrays.copyOfRange(lines, lines.length - 6 - shift, lines.length - shift));
+  }
 }
